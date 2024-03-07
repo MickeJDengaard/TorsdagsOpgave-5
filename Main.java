df98d8639d1cac7158ce71d3d3ae4fcd3f026869
MickeJDengaard <158359543+MickeJDengaard@users.noreply.github.com>
2024-03-07 20:08:49 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,24 +1,50 @@
-package TaskOne;
+package TaskThree;
+
 import java.util.ArrayList;
-class main {
 
-
+public class Main {
     public static void main(String[] args) {
-        ArrayList<Customer> customers = new ArrayList<>();
-        customers.add(new Customer("Micke", "Dengaard", "Kongen"));
-        customers.add(new Customer("Melanie", "Christensen", "Suresen"));
 
-        System.out.println(customers);
+        ArrayList<Room> rooms = new ArrayList<>();
+
+        Room room1 = new Room(1, 8, 3);
+        Room room2 = new Room(3, 2, 1);
+        Room room3 = new Room(2, 2, 2);
+
+        rooms.add(room1);
+        rooms.add(room2);
+        rooms.add(room3);
+
+        Building building1 = new Building(rooms, 2, 1, true);
+
+        int totalLamps = countLampsInBuilding(building1);
+        System.out.println("There is " + totalLamps + " lamps in the building");
+
+        isNormal(building1);
 
     }
 
-    public static void printCustomers(ArrayList<Customer> customers) {
-        for (Customer customer : customers) {
-            System.out.println(customer);
+    public static int countLampsInBuilding(Building building) {
+        int totalLamps = 0;
+        for (Room room : building.getRooms()) {
+            totalLamps += room.getNumberOfLamps();
+        }
+        return totalLamps;
+ }
+
+    public static boolean isNormal(Building building){
+        int i = 0;
+        for (Room room : building.getRooms())   {
+            i += building.getnumberOfFloors();
+        }
+        if (i > building.getRooms().size())
+        return true;
+     else {
+            System.out.println("This is an odd building");
+        return false;
         }
     }
 }
-
 
 
 
